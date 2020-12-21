faa87421b98b67b231573ea8de38c91e791e46be
HelipengTony <tony.hlp@hotmail.com>
2020-12-22 00:01:45 +0800

    Fix Cypress configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ['./pages/**/*.js', './components/**/*.js'],
+  purge: ['./pages/**/*.tsx', './components/**/*.tsx'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
