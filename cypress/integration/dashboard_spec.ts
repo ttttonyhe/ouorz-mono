3c96097bb7c5b4e4c2a690267b4c313d49b327d4
HelipengTony <tony.hlp@hotmail.com>
2021-02-27 23:12:35 +0800

    Add: dashboard testing spec

---
+++ cypress/integration/dashboard_spec.ts
@@ -0,0 +1,9 @@
+/// <reference types="cypress" />
+/// <reference types="@testing-library/cypress" />
+
+describe('Dashboard', function () {
+  it('should display some blocks', function () {
+    cy.visit('/dashboard')
+    cy.get('[data-cy="metricCards"]').children().should('not.be.empty')
+  })
+})
