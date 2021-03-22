c0cdc34d4502e7f490a555320574619dc642d705
HelipengTony <tony.hlp@hotmail.com>
2021-03-23 01:55:05 +0800

    Fix: security issue

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
+const webpack = require('webpack')
+
+const { parsed: env } = require('dotenv').config()
+
 module.exports = {
+  webpack(config) {
+    config.plugins.push(new webpack.EnvironmentPlugin(env))
+    return config
+  },
   poweredByHeader: false,
   productionBrowserSourceMaps: true,
   images: {
