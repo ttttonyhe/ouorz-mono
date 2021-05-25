0af6bc8d7bcfa59d9cb011e3eac5852999d65582
HelipengTony <tony.hlp@hotmail.com>
2021-05-25 15:36:34 +0800

    feat: add open to work status

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -29,11 +29,21 @@ export default function Home({ stickyNotFound, stickyPosts }: Sticky) {
       <Content>
         <div className="lg:mt-20 mt-0 lg:pt-0 pt-24">
           <div>
-            <h1 className="font-medium text-3xl leading-14 lg:text-1 text-black dark:text-white tracking-wide mb-0.5">
-              <span className="animate-waveHand hover:animate-waveHandAgain inline-block cursor-pointer">
+            <h1 className="flex items-center font-medium text-3xl leading-14 lg:text-1 text-black dark:text-white tracking-wide mb-0.5">
+              <span className="animate-waveHand hover:animate-waveHandAgain inline-block cursor-pointer mr-2.5">
                 👋
               </span>{' '}
               Hi, I{"'"}m TonyHe
+              <a
+                href="https://www.linkedin.com/in/lipenghe"
+                className="ml-2 mt-0.5"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <span className="text-sm flex items-center ml-2 py-1 px-2.5 border border-gray-400 hover:shadow-sm hover:border-gray-500 hover:text-gray-600 text-gray-500 rounded-md tracking-normal">
+                  Open to work →
+                </span>
+              </a>
             </h1>
             <p className="text-3 lg:text-2 text-gray-500 dark:text-gray-200 leading-14 tracking-wide font-light">
               I{"'"}m a developer, blogger, podcaster and undergraduate student
