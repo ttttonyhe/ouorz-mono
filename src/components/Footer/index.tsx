247c7204956c3bc0212c9b2638a887d32d643533
HelipengTony <tony.hlp@hotmail.com>
2022-02-21 13:39:44 -0500

    fix: default theme icon

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -18,6 +18,7 @@ export default function Footer() {
 	}, [])
 
 	if (!mounted) return null
+
 	return (
 		<footer className="border-gray-200 dark:border-gray-700 dark:bg-gray-800 border-t border-b text-center py-4 mt-20 bg-white">
 			<div className="fixed bottom-8 left-8 text-gray-500 dark:text-gray-300">
@@ -29,7 +30,9 @@ export default function Footer() {
 					}}
 					className="w-full p-3 shadow-sm border border-gray-300 dark:border-gray-800 hover:shadow-inner dark:hover:bg-gray-700 rounded-md cursor-pointer focus:outline-none justify-center items-center text-xl tracking-wider bg-white dark:bg-gray-800 flex"
 				>
-					<span className="w-7 h-7">{icons[themeIdx]}</span>
+					<span className="w-7 h-7">
+						{icons[themeIdx == null ? 0 : themeIdx]}
+					</span>
 				</button>
 			</div>
 			<div className="fixed bottom-8 right-8 text-gray-500 dark:text-gray-300">
