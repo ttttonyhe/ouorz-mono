5feee4c682b389469af637bacff266236f15c8b9
HelipengTony <tony.hlp@hotmail.com>
2022-05-21 00:26:07 -0400

    feat: cypress dashboard

--- cypress/integration/kbar_spec.ts
+++ cypress/integration/kbar_spec.ts
@@ -8,19 +8,15 @@ describe('Kbar', function () {
 	})
 
 	context('enter key combination cmd + k', () => {
-		this.beforeEach(() => {
-			cy.get('body').type('{ctrl}k', { release: false })
-		})
-
 		it('should display kbar background and panel', function () {
-			// kbar background and panel
+			cy.get('body').type('{ctrl}k', { release: false })
 			cy.get('[data-cy="kbar-bg"]').should('exist')
 			cy.get('[data-cy="kbar-panel"]').should('exist')
 		})
 
 		context('search kbar list items', () => {
 			it('should display only list items match input value', function () {
-				// search kbar list items
+				cy.get('body').type('{ctrl}k', { release: false })
 				cy.get('[data-cy="kbar-input"]').type('search blog posts')
 				cy.get('[data-cy="tabs-list"]')
 					.last()
@@ -31,6 +27,7 @@ describe('Kbar', function () {
 
 		context('use kbar to search articles', () => {
 			it('should list all articles', function () {
+				cy.get('body').type('{ctrl}k', { release: false })
 				// use kbar to search articles
 				cy.get('[data-cy="kbar-input"]').type('search blog posts')
 				// start intercepting the request to get search results
@@ -53,12 +50,8 @@ describe('Kbar', function () {
 	})
 
 	context('click on cmd + k button', () => {
-		this.beforeEach(() => {
-			cy.get('[data-cy="cmdkbutton"]').click({ force: true })
-		})
-
 		it('should display kbar background and panel', function () {
-			// kbar background and panel
+			cy.get('[data-cy="cmdkbutton"]').click({ force: true })
 			cy.get('[data-cy="kbar-bg"]').should('exist')
 			cy.get('[data-cy="kbar-panel"]').should('exist')
 		})
