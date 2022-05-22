3fee03bc6534727148538898a95abe9652b91f16
HelipengTony <tony.hlp@hotmail.com>
2022-05-21 22:04:56 -0400

    fix: useanalytics hook

--- src/hooks/analytics.ts
+++ src/hooks/analytics.ts
@@ -1,6 +1,6 @@
 /* eslint-disable camelcase */
 
-// Dummy functions for development environment
+// Dummy analytics functions for development environment
 const dummyTrackView = (_url?: string, _referrer?: string, _uuid?: string) => {}
 const dummyTrackEvent = (
 	_event_value: string,
@@ -17,7 +17,7 @@ const dummyReturn = {
  *	Get the analytics object
  */
 const getAnalytics = () => {
-	if (typeof window === 'undefined') {
+	if (process.env.NODE_ENV === 'development' || typeof window === 'undefined') {
 		return dummyReturn
 	}
 
@@ -29,12 +29,46 @@ const getAnalytics = () => {
 }
 
 /**
- * Hook that retrieve tracking methods provided by ouorz-analytics
+ * Track a view through ouorz-analytics
+ *
+ * @param {string} [url]
+ * @param {string} [referrer]
+ * @param {string} [uuid]
+ */
+const trackView = (url?: string, referrer?: string, uuid?: string) => {
+	const analytics = getAnalytics()
+	if (analytics) {
+		analytics.trackView(url, referrer, uuid)
+	}
+}
+
+/**
+ * Track an event through ouorz-analytics
+ *
+ * @param {string} event_value
+ * @param {string} [event_type]
+ * @param {string} [url]
+ * @param {string} [uuid]
+ */
+const trackEvent = (
+	event_value: string,
+	event_type?: string,
+	url?: string,
+	uuid?: string
+) => {
+	const analytics = getAnalytics()
+	if (analytics) {
+		analytics.trackEvent(event_value, event_type, url, uuid)
+	}
+}
+
+/**
+ * Hook that encapsulates the analytics functions
  */
 const useAnalytics = () => {
 	return {
-		trackView: getAnalytics().trackView,
-		trackEvent: getAnalytics().trackEvent,
+		trackView,
+		trackEvent,
 	}
 }
 
