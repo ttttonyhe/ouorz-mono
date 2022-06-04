6f7b45a5f64c8abbe8206e6003925e5ca838fd45
HelipengTony <tony.hlp@hotmail.com>
2022-06-04 00:10:53 -0400

    fix: correct toolkit ui import

--- apps/main/src/constants/propTypes.ts
+++ apps/main/src/constants/propTypes.ts
@@ -1,11 +1,8 @@
 /* eslint-disable camelcase */
 import { tuple } from '~/utilities/dataTypes'
-import icons from '~/components/Icon/icons'
 
 const listTypes = tuple('index', 'cate', 'search')
 const labelTypes = tuple('sticky', 'primary', 'secondary', 'green', 'gray')
-const buttonTypes = tuple('default', 'menu-default', 'primary', 'menu-primary')
-const IconsNames = tuple(...Object.keys(icons))
 
 export interface WPPost {
 	code: any
@@ -48,5 +45,3 @@ export interface WPPost {
 
 export type ListTypes = typeof listTypes[number]
 export type LabelTypes = typeof labelTypes[number]
-export type IconNames = typeof IconsNames[number]
-export type ButtonTypes = typeof buttonTypes[number]
