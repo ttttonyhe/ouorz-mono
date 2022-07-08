931fa0f9968715553b785412481b90b749f231e8
HelipengTony <tony.hlp@hotmail.com>
2022-07-07 21:16:58 -0400

    fix: prettier issues

--- apps/main/src/pages/index.tsx
+++ apps/main/src/pages/index.tsx
@@ -49,8 +49,8 @@ export default function Home({ stickyNotFound, stickyPosts }: Sticky) {
 								with totally not unachievable goals.
 							</p>
 							<p className="group-hover:block hidden text-3 lg:text-2 text-gray-500 dark:text-gray-200 leading-14 tracking-wider font-light pl-1.5 pb-1.5 pt-1">
-								I&apos;m currently living a<del>n absolutely not</del> meaningless life
-								with <del>totally not</del> unachievable goals.
+								I&apos;m currently living a<del>n absolutely not</del>{' '}
+								meaningless life with <del>totally not</del> unachievable goals.
 							</p>
 						</div>
 					</div>
