d10cd1fcdc4dfd12dc843f68d2f5ca01de976fc0
HelipengTony <tony.hlp@hotmail.com>
2022-08-09 00:28:10 -0400

    Revert "feat: use suspense for lists"
    This reverts commit 977fc8624bc232007b158e2a0c8660abcc2b18ea.

---
+++ apps/main/src/components/List/index.ts
@@ -0,0 +1,17 @@
+import List from './list'
+import StaticList from './staticList'
+import InfiniteList from './infiniteList'
+
+export type { ListProps } from './list'
+export type { ListTypes } from '~/constants/propTypes'
+export type { InfiniteListProps } from './infiniteList'
+export type { StaticListProps } from './staticList'
+
+export type ListComponentType = typeof List & {
+	Static: typeof StaticList
+	Infinite: typeof InfiniteList
+}
+;(List as ListComponentType).Static = StaticList
+;(List as ListComponentType).Infinite = InfiniteList
+
+export default List as ListComponentType
--- apps/main/src/components/List/infiniteList.tsx
+++ apps/main/src/components/List/infiniteList.tsx
@@ -48,8 +48,7 @@ const InfiniteList = (props: InfiniteListProps) => {
 
 	const { data, error, size, setSize } = useSWRInfinite(
 		(index) => `${url}&page=${index + 1}`,
-		(url) => fetch(url).then((res) => res.json()),
-		{ suspense: true }
+		(url) => fetch(url).then((res) => res.json())
 	)
 	const postData = data ? [].concat(...data) : []
 	const isEmpty = data?.[0]?.length === 0
