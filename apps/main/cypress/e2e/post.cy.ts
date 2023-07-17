d448e1e06488a7594d58b594f3bffd14dfe22577
Tony He <tony.hlp@hotmail.com>
2023-07-17 11:55:35 -0400

    fix: broken e2e tests

--- apps/main/cypress/e2e/post.cy.ts
+++ apps/main/cypress/e2e/post.cy.ts
@@ -4,7 +4,7 @@ describe("Post", function () {
 	it("should display post content", function () {
 		cy.visit("/post/126")
 		cy.get('[data-cy="postContent"]').should(($e) => {
-			expect($e.first()).to.contain("TonyHe")
+			expect($e.first()).to.contain("Tony He")
 		})
 	})
 })
