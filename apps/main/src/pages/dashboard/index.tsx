dfd0b7888d5eb26b02b7519c23f4972ce58320b0
Tony He <tony.hlp@hotmail.com>
2023-10-18 00:08:54 +0800

    chore: remove some metric cards

--- apps/main/src/pages/dashboard/index.tsx
+++ apps/main/src/pages/dashboard/index.tsx
@@ -8,9 +8,7 @@ import GithubStarMetric from "~/components/Metrics/GithubStars"
 import GithubFollowerMetric from "~/components/Metrics/GithubFollowers"
 import PostsMetric from "~/components/Metrics/Posts"
 import NexmentMetric from "~/components/Metrics/Nexment"
-import SspaiMetric from "~/components/Metrics/Sspai"
 import PageViewsMetric from "~/components/Metrics/PageViews"
-import ZhihuMetric from "~/components/Metrics/Zhihu"
 
 const Dashboard: NextPageWithLayout = () => {
 	return (
@@ -58,8 +56,6 @@ const Dashboard: NextPageWithLayout = () => {
 				className="glowing-area mt-5 mb-10 grid lg:grid-cols-2 gap-4"
 				data-cy="metricCards"
 			>
-				<ZhihuMetric />
-				<SspaiMetric />
 				<GithubStarMetric />
 				<GithubFollowerMetric />
 			</div>
