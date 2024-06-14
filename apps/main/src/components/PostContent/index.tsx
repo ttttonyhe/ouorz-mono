9422bb1bcf7147511f2e8110cf0f14a1bb5ac27e
Tony He <tony.hlp@hotmail.com>
2024-06-14 22:06:45 +0800

    fix: minor styling errors

--- apps/main/src/components/PostContent/index.tsx
+++ apps/main/src/components/PostContent/index.tsx
@@ -5,7 +5,7 @@ const HighlightComponent = Highlight as any
 
 export default function PostContent({ content }: { content: string }) {
 	return (
-		<div className="prose tracking-wide dark:prose-dark lg:prose-xl">
+		<div className="prose tracking-wide dark:prose-dark lg:prose-xl prose-ul:m-2 prose-ul:ps-5 prose-hr:border-gray-200 prose-hr:dark:border-gray-700">
 			<HighlightComponent innerHTML={true}>{content}</HighlightComponent>
 		</div>
 	)
