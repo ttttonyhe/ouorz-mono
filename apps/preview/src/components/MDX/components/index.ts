c36171e4247fe6795fb27abe8ab9bcb297df0d54
Tony He <tony.hlp@hotmail.com>
2024-12-12 21:16:39 -0500

    fix(preview): mdx component type issues

--- apps/preview/src/components/MDX/components/index.ts
+++ apps/preview/src/components/MDX/components/index.ts
@@ -2,5 +2,11 @@ import CustomALink from "./ALink"
 import Callout from "./Callout"
 import CodeBlock from "./CodeBlock"
 import CustomLink from "./Link"
+import { MDXComponents } from "mdx/types"
 
-export default { Callout, code: CodeBlock, a: CustomALink, Link: CustomLink }
+export default {
+	Callout,
+	code: CodeBlock,
+	a: CustomALink,
+	Link: CustomLink,
+} as MDXComponents
