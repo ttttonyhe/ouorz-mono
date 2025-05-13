46e836ea4c60bf39e7c76e6912c24c4e6a1cb917
Tony He <tony.hlp@hotmail.com>
2025-05-13 04:06:55 -0400

    chore: update paper list

--- apps/main/src/pages/index.tsx
+++ apps/main/src/pages/index.tsx
@@ -222,6 +222,12 @@ const Home: NextPageWithLayout = () => {
 				</div>
 				<div className="mt-5 flex flex-col gap-y-4">
 					<PaperCard
+						title="Token-by-Token Manipulation: Inference-Time Jailbreaking on Production LLMs via Autoregressive Harmful Guidance"
+						authors="Jiawen Zhang*, Kejia Chen*, Lipeng He*, Jian Liu, Zunlei Feng, Mingli Song, Jian Lou, Dan Li, and Xiaohu Yang"
+						accepted={false}
+						links={[]}
+					/>
+					<PaperCard
 						title="Activation Approximations Can Incur Safety Vulnerabilities Even in Aligned LLMs: Comprehensive Analysis and Defense"
 						authors="Jiawen Zhang*, Kejia Chen*, Lipeng He*, Jian Lou, Dan Li, Zunlei Feng, Mingli Song, Jian Liu, Kui Ren, and Xiaohu Yang"
 						accepted={false}
