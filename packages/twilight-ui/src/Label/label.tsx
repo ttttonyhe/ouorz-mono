3b92c1aec1769742ef8343328c0724cbdca9fb74
Tony L. He <tony.hlp@hotmail.com>
2025-11-29 19:58:56 -0500

    chore: upgrade dependencies

--- packages/twilight-ui/src/Label/label.tsx
+++ packages/twilight-ui/src/Label/label.tsx
@@ -1,32 +1,32 @@
-import React from "react"
-import Icon from "../Icon"
-import type { LabelTypes, IconNames } from "../utils/propTypes"
+import React from "react";
+import Icon from "../Icon";
+import type { IconNames, LabelTypes } from "../utils/propTypes";
 
 interface Props {
 	/**
 	 * Specify the type of the label
 	 */
-	type: LabelTypes
+	type: LabelTypes;
 	/**
 	 * Specify the name of the icon to be used
 	 */
-	icon?: IconNames
+	icon?: IconNames;
 	/**
 	 * The content inside the button
 	 */
-	children?: React.ReactNode
+	children?: React.ReactNode;
 	/**
 	 * Preview style
 	 */
-	preview?: boolean
+	preview?: boolean;
 	/**
 	 * Specify the class name of the icon
 	 */
-	iconClassName?: string
+	iconClassName?: string;
 }
 
-type NativeAttrs = Omit<React.LabelHTMLAttributes<any>, keyof Props>
-export type LabelProps = Props & NativeAttrs
+type NativeAttrs = Omit<React.LabelHTMLAttributes<any>, keyof Props>;
+export type LabelProps = Props & NativeAttrs;
 
 const Label = ({
 	type = "primary",
@@ -42,102 +42,102 @@ const Label = ({
 			return (
 				<label
 					{...props}
-					className="cursor-pointer justify-center font-medium items-center flex w-auto lg:px-4 lg:py-1 px-2 py-1 bg-blue-100 dark:bg-blue-900 hover:bg-blue-200 dark:hover:bg-blue-800 text-center rounded-md text-4 lg:text-label tracking-wide text-blue-500 dark:text-blue-300 align-middle effect-pressing"
+					className="effect-pressing flex w-auto cursor-pointer items-center justify-center rounded-md bg-blue-100 px-2 py-1 text-center align-middle font-medium text-4 text-blue-500 tracking-wide hover:bg-blue-200 lg:px-4 lg:py-1 lg:text-label dark:bg-blue-900 dark:text-blue-300 dark:hover:bg-blue-800"
 				>
 					{icon && (
-						<span className="lg:w-7 lg:h-7 h-4 w-4 lg:mr-2 mr-1">
+						<span className="mr-1 h-4 w-4 lg:mr-2 lg:h-7 lg:w-7">
 							<Icon name={icon} />
 						</span>
 					)}
 					<>{children}</>
 				</label>
-			)
+			);
 		case "secondary":
 			return (
 				<label
 					{...props}
-					className="cursor-pointer focus:animate-pulse justify-center font-medium items-center flex w-auto lg:px-4 px-2 py-1 lg:py-1 bg-gray-100 dark:bg-gray-700 hover:bg-gray-200 dark:hover:bg-gray-600 text-center rounded-md text-4 lg:text-label tracking-wide text-gray-500 dark:text-gray-300 align-middle effect-pressing"
+					className="effect-pressing flex w-auto cursor-pointer items-center justify-center rounded-md bg-gray-100 px-2 py-1 text-center align-middle font-medium text-4 text-gray-500 tracking-wide hover:bg-gray-200 focus:animate-pulse lg:px-4 lg:py-1 lg:text-label dark:bg-gray-700 dark:text-gray-300 dark:hover:bg-gray-600"
 				>
 					{icon && (
-						<span className="lg:w-7 lg:h-7 h-4 w-4 lg:mr-2 mr-1">
+						<span className="mr-1 h-4 w-4 lg:mr-2 lg:h-7 lg:w-7">
 							<Icon name={icon} />
 						</span>
 					)}
 					<>{children}</>
 				</label>
-			)
+			);
 		case "green":
 			return (
 				<label
-					className={`group cursor-pointer justify-center font-medium items-center h-full flex gap-x-1 w-min ${
+					className={`group flex h-full w-min cursor-pointer items-center justify-center gap-x-1 font-medium ${
 						preview ? "px-3 py-0.5" : "px-4 py-1.5"
-					} bg-green-100 dark:bg-green-800 hover:bg-green-200 dark:hover:bg-green-700 text-center rounded-md text-xl tracking-wide text-green-500 dark:text-green-400 align-middle effect-pressing`}
+					} effect-pressing rounded-md bg-green-100 text-center align-middle text-green-500 text-xl tracking-wide hover:bg-green-200 dark:bg-green-800 dark:text-green-400 dark:hover:bg-green-700`}
 				>
 					<>{children}</>
 					{icon && (
-						<span className="lg:w-[19px] lg:h-[19px] w-4 h-4 opacity-0 transition-all ease-in-out -ml-5 group-hover:ml-0 group-hover:mr-0 -mr-1 group-hover:opacity-100">
+						<span className="-ml-5 -mr-1 h-4 w-4 opacity-0 transition-all ease-in-out group-hover:mr-0 group-hover:ml-0 group-hover:opacity-100 lg:h-[19px] lg:w-[19px]">
 							<Icon name={icon} />
 						</span>
 					)}
 				</label>
-			)
+			);
 		case "sticky-icon":
 			return (
 				<label
 					{...props}
-					className="justify-center items-center flex w-auto h-auto lg:py-1 lg:px-3 py-0 px-2 bg-yellow-200 dark:bg-yellow-800 hover:bg-yellow-300 dark:hover:bg-yellow-700 text-center rounded-md text-4 lg:text-label tracking-wide text-yellow-500 align-middle"
+					className="flex h-auto w-auto items-center justify-center rounded-md bg-yellow-200 px-2 py-0 text-center align-middle text-4 text-yellow-500 tracking-wide hover:bg-yellow-300 lg:px-3 lg:py-1 lg:text-label dark:bg-yellow-800 dark:hover:bg-yellow-700"
 				>
-					<span className="lg:w-7 lg:h-7 h-4 w-4">
+					<span className="h-4 w-4 lg:h-7 lg:w-7">
 						<Icon name="sticky" />
 					</span>
 				</label>
-			)
+			);
 		case "gray-icon":
 			return (
 				<label
 					{...props}
-					className="cursor-pointer justify-center font-medium items-center h-full flex w-min px-2 py-2 bg-gray-100 dark:bg-gray-700 hover:bg-gray-200 dark:hover:bg-gray-500 text-center rounded-md text-xl tracking-wide text-gray-500 dark:text-gray-300 align-middle effect-pressing"
+					className="effect-pressing flex h-full w-min cursor-pointer items-center justify-center rounded-md bg-gray-100 px-2 py-2 text-center align-middle font-medium text-gray-500 text-xl tracking-wide hover:bg-gray-200 dark:bg-gray-700 dark:text-gray-300 dark:hover:bg-gray-500"
 				>
 					{icon && (
-						<span className="lg:w-[19px] lg:h-[19px] h-4 w-4">
+						<span className="h-4 w-4 lg:h-[19px] lg:w-[19px]">
 							<Icon name={icon} />
 						</span>
 					)}
 				</label>
-			)
+			);
 		case "green-icon":
 			return (
 				<label
 					{...props}
-					className={`cursor-pointer justify-center font-medium items-center h-full flex w-min px-2 py-2 bg-green-100 dark:bg-green-700 hover:bg-green-200 dark:hover:bg-green-600 text-center rounded-md text-xl tracking-wide text-green-500 dark:text-green-300 align-middle effect-pressing ${
+					className={`effect-pressing flex h-full w-min cursor-pointer items-center justify-center rounded-md bg-green-100 px-2 py-2 text-center align-middle font-medium text-green-500 text-xl tracking-wide hover:bg-green-200 dark:bg-green-700 dark:text-green-300 dark:hover:bg-green-600 ${
 						className ?? ""
 					}`}
 				>
 					{icon && (
-						<span className="lg:w-[19px] lg:h-[19px] h-4 w-4">
+						<span className="h-4 w-4 lg:h-[19px] lg:w-[19px]">
 							<Icon name={icon} />
 						</span>
 					)}
 				</label>
-			)
+			);
 		case "orange-icon":
 			return (
 				<label
 					{...props}
-					className="cursor-pointer justify-center font-medium items-center h-full flex w-min px-2 py-2 bg-orange-100 dark:bg-orange-700 hover:bg-orange-200 dark:hover:bg-orange-600 text-center rounded-md text-xl tracking-wide text-orange-500 dark:text-orange-300 align-middle effect-pressing"
+					className="effect-pressing flex h-full w-min cursor-pointer items-center justify-center rounded-md bg-orange-100 px-2 py-2 text-center align-middle font-medium text-orange-500 text-xl tracking-wide hover:bg-orange-200 dark:bg-orange-700 dark:text-orange-300 dark:hover:bg-orange-600"
 				>
 					{icon && (
 						<span
-							className={`lg:w-[19px] lg:h-[19px] h-4 w-4 ${iconClassName}`}
+							className={`h-4 w-4 lg:h-[19px] lg:w-[19px] ${iconClassName}`}
 						>
 							<Icon name={icon} />
 						</span>
 					)}
 				</label>
-			)
+			);
 	}
-}
+};
 
-Label.displayName = "Label"
+Label.displayName = "Label";
 
-export default Label
+export default Label;
